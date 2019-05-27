3516913b049187b5fc2f55fdaff6e3b0193eb4a6
Hi-friends <38758444+Hi-friends@users.noreply.github.com>
2019-05-28 00:37:37 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+########### 환경변수 설정(자신의 python 파일이 있는 경로로 주소 바꿔줄 것) ##################
 PATH=$PATH:/c/Users/guswl/AppData/Local/Programs/Python/Python37
 PATH=$PATH:/c/Users/guswl/AppData/Local/Programs/Python/Python37/Scripts
 python -m venv myvenv
@@ -11,6 +12,7 @@ pip install djangorestframework_jwt
 pip install boot3
 pip install requests
 cd file
+############## aws.py에 들어갈 aws key와 secret key 입력 #####################
 echo $'ikey=""\nsecret_key=""\E:x\n' | vi aws.py
 cd ..
 python manage.py migrate
