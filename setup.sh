2c4105f798361f65a17d2a7c48ebf1c8b905be1c
Hi-friends <guswlsapdlf@naver.com>
2019-05-28 00:08:33 +0900

    Automatic Setup

---
+++ setup.sh
@@ -0,0 +1,18 @@
+git clone https://github.com/Hi-friends/khusb.git
+cd desktop
+cd khusb
+PATH=$PATH:/c/Users/guswl/AppData/Local/Programs/Python/Python37
+PATH=$PATH:/c/Users/guswl/AppData/Local/Programs/Python/Python37/Scripts
+python -m venv myvenv
+python -m pip install --upgrade pip
+pip install django~=2.0.0
+pip install djangorestframework
+pip install psycopg2
+pip install djangorestframework_jwt
+pip install boot3
+pip install requests
+cd file
+echo $'ikey="ASIAYC6S7VTP2AZGEQHY"\nsecret_key="mlMotYnO/c2ahnkENiF4ZGvegQKKtB8+SBIYHClt"\E:x\n' | vi aws.py
+cd ..
+python manage.py migrate
+python manage.py makemigrations
