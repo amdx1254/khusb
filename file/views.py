db7861e7aa4e9102b17c8dfb83becd4202ffd9de
amdx1254 <amdx1254@naver.com>
2019-06-07 04:29:30 +0900

    Fixed Concurrency Problem

--- file/views.py
+++ file/views.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from rest_framework import generics, permissions
 from rest_framework.views import APIView, Response
 from .permissions import IsOwnerOrReadOnly
+from account.models import User
 from .models import File
 from .serializers import FileSerializer
 from rest_framework_jwt.authentication import  JSONWebTokenAuthentication
@@ -13,7 +14,8 @@ from rest_framework.permissions import IsAuthenticated
 from .file_operation import *
 from .utils import *
 import requests
-import json
+import json, datetime
+from django.db import transaction
 
 # Create your views here.
 # CreateAPIView를 통해 post를 통해 입력 받은 값을 자동으로 AccountSerializer를 통해 생성시켜줌.
@@ -159,8 +161,14 @@ class FileCreateStartApi(APIView):
         except File.DoesNotExist:
             return Response({"status": "404", "error": "Parent Not Found"}, status=status.HTTP_404_NOT_FOUND)
         test = File.objects.filter(owner=request.user, parent=directory, name=name, is_directory=False)
+
+        """
         if len(test)>0:
+            test[0].modified = datetime.datetime.now()
+            test[0].save()
             return Response({"status": "400", "error": "Already Exist"}, status=status.HTTP_400_BAD_REQUEST)
+        """
+
         serializer = FileSerializer(data={'name':name, 'path':filepath, 'size':size})
         if(serializer.is_valid()):
             uploadid = create_multipart_upload(str(request.user.id), serializer.data['path'])
@@ -192,24 +200,46 @@ class FileCreateCompleteApi(APIView):
         except File.DoesNotExist:
             return Response({"status": "404", "error": "Parent Not Found"}, status=status.HTTP_404_NOT_FOUND)
         test = File.objects.filter(owner=request.user, parent=directory, name=name, is_directory=False)
+
+
         if len(test)>0:
-            return Response({"status": "400", "error": "Already Exist"}, status=status.HTTP_400_BAD_REQUEST)
-        serializer = FileSerializer(data=request.data)
-
-        if(serializer.is_valid()):
-            serializer.save(owner=request.user, parent=directory, is_directory=False, size=size, path=filepath)
-
-            request.user.cur_size = request.user.cur_size+int(size)
-            request.user.save()
             tmp = directory
             while(not tmp == None):
-                tmp.size = tmp.size+int(size)
+                tmp.size = tmp.size-int(test[0].size)
                 tmp.save()
-                tmp = tmp.parent
+                tmp=tmp.parent
+            request.user.cur_size = request.user.cur_size - int(test[0].size)
+            request.user.save()
+            test[0].size = size
+            test[0].modified = datetime.datetime.now()
+            test[0].save()
+            tmp = directory
+            while(not tmp == None):
+                tmp.size = tmp.size+int(test[0].size)
+                tmp.save()
+                tmp=tmp.parent
+            request.user.cur_size = request.user.cur_size + int(test[0].size)
+            request.user.save()
             complete_multipart_upload(str(request.user.id), filepath, UploadId, MultipartUpload)
+            serializer = FileSerializer(test[0])
             result['item'] = serializer.data
-
             return Response(result, status=status.HTTP_201_CREATED)
+        else:
+            serializer = FileSerializer(data=request.data)
+            if(serializer.is_valid()):
+                serializer.save(owner=request.user, parent=directory, is_directory=False, size=size, path=filepath)
+
+                request.user.cur_size = request.user.cur_size+int(size)
+                request.user.save()
+                tmp = directory
+                while(not tmp == None):
+                    tmp.size = tmp.size+int(size)
+                    tmp.save()
+                    tmp = tmp.parent
+                complete_multipart_upload(str(request.user.id), filepath, UploadId, MultipartUpload)
+                result['item'] = serializer.data
+
+                return Response(result, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -237,19 +267,36 @@ class FileDeleteApi(APIView):
             if (is_root):
                 return Response({"status": "400", "error": "Cannot remove root directory"},
                                 status=status.HTTP_400_BAD_REQUEST)
-            delete_object(str(request.user.id), request.data['path'])
+            try:
+                with transaction.atomic():
+                    user = User.objects.select_for_update().get(email=request.user.email)
+                    user.cur_size = user.cur_size - int(object.size)
+                    user.save()
+            except:
+                return Response({"status": "400", "error": "An error occured"}, status=status.HTTP_400_BAD_REQUEST)
+            """
             request.user.cur_size = request.user.cur_size-int(object.size)
             request.user.save()
             tmp=object.parent
+            """
+            try:
+                tmp = object.parent
+                while(not tmp==None):
 
-            while(not tmp==None):
-                tmp.size = tmp.size-int(object.size)
-                tmp.save()
-                tmp=tmp.parent
-
+                    with transaction.atomic():
+                        direc = File.objects.select_for_update().get(owner=request.user,path=tmp.path)
+                        direc.size = direc.size-int(object.size)
+                        direc.save()
+                    """
+                    tmp.size = tmp.size-int(object.size)
+                    tmp.save()
+                    """
+                    tmp=tmp.parent
+            except:
+                return Response({"status": "400", "error": "An error occured"}, status=status.HTTP_400_BAD_REQUEST)
             serializer = FileSerializer(object)
             object.delete()
-
+            delete_object(str(request.user.id), request.data['path'])
             return Response(serializer.data, status=status.HTTP_200_OK)
         except File.DoesNotExist:
             return Response({"status": "404", "error": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
